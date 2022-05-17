c816d55f8758911f793babb50f0c8051f9f9acbf
MJKRIS <105255164+MJKRIS@users.noreply.github.com>
2022-05-16 20:06:23 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,45 +1,53 @@
-from socket import socket, error
+#Entradas
+passw = "AyPP2022"
+for i in range (0,3):
+    pwd = input("dame la contraseña\n")
+    if pwd == passw:
+        print ("la contraseña es correcta")
+        from socket import socket, error
 
-import nacl.utils
-from nacl.public import PrivateKey, SealedBox
-import getpass
+        import nacl.utils
+        from nacl.public import PrivateKey, SealedBox
+        import getpass
 
-skfile = PrivateKey.generate()
-pkfile = skfile.public_key
+        skfile = PrivateKey.generate()
+        pkfile = skfile.public_key
 
-sealed_box = SealedBox(pkfile)
+        sealed_box = SealedBox(pkfile)
 
-def main():
-    s = socket()
+        def main():
+            s = socket()
     
-    #puerto 6030
-    s.bind(("localhost", 6030))
-    s.listen(0)
+            #puerto 6030
+            s.bind(("localhost", 6030))
+            s.listen(0)
     
-    conn, addr = s.accept()
-    f = open("captura-recibido.png", "wb")
+            conn, addr = s.accept()
+            f = open("captura-recibido.png", "wb")
     
-    while True:
-        try:
-            input_data = conn.recv(1024)
-        except error:
-            print("Error")
-            break
-        else:
-            if input_data:
-                if isinstance(input_data, bytes):
-                    end = input_data[0] == 1
-                else:
-                    end = input_data == chr(1)
-                if not end:
-                    # Almacenar datos.
-                    encrypted = sealed_box.encrypt(input_data)
-                    f.write(encrypted)
-                else:
+            while True:
+                try:
+                    input_data = conn.recv(1024)
+                except error:
+                    print("Error")
                     break
+                else:
+                    if input_data:
+                        if isinstance(input_data, bytes):
+                            end = input_data[0] == 1
+                        else:
+                            end = input_data == chr(1)
+                        if not end:
+                            # Almacenar datos.
+                            encrypted = sealed_box.encrypt(input_data)
+                            f.write(encrypted)
+                        else:
+                            break
     
-    print("Exito")
-    f.close()
+            print("Exito")
+            f.close()
 
-if __name__ == "__main__":
-    main()
+        if __name__ == "__main__":
+            main()
+    if i == 2:
+        print("agotaste los 3 intentos")

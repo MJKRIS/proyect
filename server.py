95b2e7efb096c048c9414f755f77be1f850304c7
MJKRIS <105255164+MJKRIS@users.noreply.github.com>
2022-05-16 20:00:16 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,45 @@
+from socket import socket, error
+
+import nacl.utils
+from nacl.public import PrivateKey, SealedBox
+import getpass
+
+skfile = PrivateKey.generate()
+pkfile = skfile.public_key
+
+sealed_box = SealedBox(pkfile)
+
+def main():
+    s = socket()
+    
+    #puerto 6030
+    s.bind(("localhost", 6030))
+    s.listen(0)
+    
+    conn, addr = s.accept()
+    f = open("captura-recibido.png", "wb")
+    
+    while True:
+        try:
+            input_data = conn.recv(1024)
+        except error:
+            print("Error")
+            break
+        else:
+            if input_data:
+                if isinstance(input_data, bytes):
+                    end = input_data[0] == 1
+                else:
+                    end = input_data == chr(1)
+                if not end:
+                    # Almacenar datos.
+                    encrypted = sealed_box.encrypt(input_data)
+                    f.write(encrypted)
+                else:
+                    break
+    
+    print("Exito")
+    f.close()
+
+if __name__ == "__main__":
+    main()
